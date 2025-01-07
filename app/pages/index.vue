e24815cfed4c1022c8e4cb52b0a848088ec0ef22
MyungGyu <mglee@xenialsoft.com>
2025-01-07 16:41:21 +0900

    chore

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,61 +1,58 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container mx-auto px-9">
-    <div class="hero">
-      <div class="hero-content text-center">
-        <div class="prose">
-          <h1>(주)제니얼소프트</h1>
-          <h5>
-            제니얼소프트는 웹 외주 개발을 전문으로 하고 있습니다. <br>고품질과
-            기술력을 바탕으로 개발 프로젝트를 수행하고 있습니다.
-          </h5>
-          <br>
-          <h3>수행 가능한 개발 환경</h3>
-          <div class="flex flex-col items-center gap-4 sm:flex-row">
-            <div class="flip">
-              <div class="card">
-                <div class="front">
-                  FrontEnd
-                </div>
-                <div class="back">
-                  JavaScript<br>
-                  JQuery<br>
-                  Vue
-                </div>
-              </div>
-            </div>
-            <div class="flip">
-              <div class="card">
-                <div class="front">
-                  BackEnd
-                </div>
-                <div class="back">
-                  Java<br>
-                  SpringBoot
-                </div>
-              </div>
-            </div>
-            <div class="flip">
-              <div class="card">
-                <div class="front">
-                  DB
-                </div>
-                <div class="back">
-                  Oracle<br>
-                  MariaDB<br>
-                  MySQL
-                </div>
-              </div>
-            </div>
+  <div class="container prose mx-auto text-center">
+    <h1>(주)제니얼소프트</h1>
+    <h5>
+      제니얼소프트는 웹 외주 개발을 전문으로 하고 있습니다.
+      <br>
+      고품질과 기술력을 바탕으로 개발 프로젝트를 수행하고 있습니다.
+    </h5>
+    <h3 class="mt-12">
+      수행 가능한 개발 환경
+    </h3>
+    <div class="flex flex-col items-center gap-4 sm:flex-row">
+      <div class="flip">
+        <div class="card">
+          <div class="front">
+            FrontEnd
           </div>
-          <br>
-          <h3>프로젝트 문의</h3>
-          <div>
-            xenialsoft@xenialsoft.com
+          <div class="back">
+            JavaScript<br>
+            JQuery<br>
+            Vue
           </div>
         </div>
       </div>
+      <div class="flip">
+        <div class="card">
+          <div class="front">
+            BackEnd
+          </div>
+          <div class="back">
+            Java<br>
+            SpringBoot
+          </div>
+        </div>
+      </div>
+      <div class="flip">
+        <div class="card">
+          <div class="front">
+            DB
+          </div>
+          <div class="back">
+            Oracle<br>
+            MariaDB<br>
+            MySQL
+          </div>
+        </div>
+      </div>
+    </div>
+    <h3 class="mt-12">
+      프로젝트 문의
+    </h3>
+    <div>
+      xenialsoft@xenialsoft.com
     </div>
   </div>
 </template>
@@ -83,14 +80,10 @@
 .front {
   background: rgb(245, 245, 245);
 }
-
-/* // 뒷면은 사전에 미리 뒤집기 */
 .back {
   background: #9ff7ff;
   transform: rotateY(180deg);
 }
-
-/* // 호버 시 뒤집기 */
 .flip:hover .card {
   transform: rotateY(180deg);
 }
