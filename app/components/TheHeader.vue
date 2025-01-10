cfb95bad14115eb9834b8f81e4c3e65b7264fa83
semi <smkim@xenialsoft.com>
2025-01-10 11:13:57 +0900

    index, header 수정

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <header class="fixed top-0 w-full">
-    <div class="container mx-auto flex min-h-16 items-center px-8">
+  <header class="fixed top-0 z-[999] w-full border-b-[1px] border-[#e4e4e4] bg-white">
+    <div class="container mx-auto flex min-h-16 max-w-3xl items-center px-8">
       <NuxtLink
         class="text-2xl font-bold"
         to="/"
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="container m-auto flex h-full flex-col gap-y-32 py-24 text-center">
+  <div
+    class="container m-auto flex h-full max-w-3xl flex-col gap-y-16 px-8 py-16 text-center"
+  >
+    <!-- <section> -->
     <section>
-      <h1 class="mb-8 text-3xl font-bold sm:mb-16 sm:text-6xl">
-        (주)제니얼소프트
+      <h1
+        class="mb-8 text-start text-3xl font-bold !leading-snug sm:text-5xl"
+      >
+        신뢰할 수 있는 서비스와<br>
+        시스템 통합을 경험해보세요.
       </h1>
-      <div class="flex flex-col gap-y-6 text-sm sm:text-3xl">
+      <div class="max-h-3xl max-w-3xl">
+        <img
+          src="~/assets/images/paperwork-5000691_1920.webp"
+          class="image-full rounded-2xl opacity-90"
+        >
+        <!-- <h1 class="mb-8 text-3xl font-bold sm:mb-16 sm:text-6xl">
+        (주)제니얼소프트
+      </h1> -->
+        <!-- <div class="flex flex-col gap-y-6 text-sm sm:text-3xl">
         <div>제니얼소프트는 웹 외주 개발을 전문으로 하고 있습니다.</div>
         <div>고품질과 기술력을 바탕으로 개발 프로젝트를 수행하고 있습니다.</div>
       </div>
@@ -14,6 +28,16 @@
         프로젝트 문의
       </h3>
       <div class="text-lg italic sm:text-2xl">
+        xenialsoft@xenialsoft.com
+      </div>
+    </section> -->
+      </div>
+    </section>
+    <section>
+      <div class="text-xl font-bold sm:text-3xl">
+        프로젝트 문의
+      </div>
+      <div class="text-lg italic sm:text-xl">
         xenialsoft@xenialsoft.com
       </div>
     </section>
