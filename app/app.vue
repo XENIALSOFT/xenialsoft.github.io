72b4f2607d39a95682e462c7c6fcd104bc426ce8
semi <smkim@xenialsoft.com>
2025-01-10 11:28:31 +0900

    seo추가

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-// const title = ref(`(주)제니얼소프트 | XENIALSOFT Inc.`);
-// useHead({
-//   title,
-// });
+useSeoMeta({
+  title: '(주)제니얼소프트 | XENIALSOFT Inc.',
+  ogTitle: '(주)제니얼소프트 | XENIALSOFT Inc.',
+  description: '신뢰할 수 있는 서비스와 시스템 통합을 경험해보세요.',
+  ogDescription: '신뢰할 수 있는 서비스와 시스템 통합을 경험해보세요.',
+  ogImage: 'https://xenialsoft.com/logo_light.png',
+});
 </script>
 
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
-  <!-- <NuxtWelcome :title /> -->
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
