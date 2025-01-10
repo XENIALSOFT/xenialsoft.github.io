8ac1ee83f30a2539309446b5e6e2817da14d7fa8
semi <smkim@xenialsoft.com>
2025-01-10 11:37:23 +0900

    alt 추가

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,23 +14,8 @@
         <img
           src="~/assets/images/paperwork-5000691_1920.webp"
           class="image-full rounded-2xl opacity-90"
+          alt="신뢰할 수 있는 서비스와 시스템 통합"
         >
-        <!-- <h1 class="mb-8 text-3xl font-bold sm:mb-16 sm:text-6xl">
-        (주)제니얼소프트
-      </h1> -->
-        <!-- <div class="flex flex-col gap-y-6 text-sm sm:text-3xl">
-        <div>제니얼소프트는 웹 외주 개발을 전문으로 하고 있습니다.</div>
-        <div>고품질과 기술력을 바탕으로 개발 프로젝트를 수행하고 있습니다.</div>
-      </div>
-    </section>
-    <section>
-      <h3 class="mb-6 text-2xl font-bold sm:text-4xl">
-        프로젝트 문의
-      </h3>
-      <div class="text-lg italic sm:text-2xl">
-        xenialsoft@xenialsoft.com
-      </div>
-    </section> -->
       </div>
     </section>
     <section>
