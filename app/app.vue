c8e813962059e62ad589ff16df0ac2d34b7f2eb6
MyungGyu <mglee@xenialsoft.com>
2025-01-21 12:18:40 +0900

    패키지업데이트트

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 useSeoMeta({
-  ogTitle: '(주)제니얼소프트 | XENIALSOFT Inc.',
+  ogTitle: '(주)제니얼소프트',
   ogDescription: '신뢰할 수 있는 서비스와 시스템 통합을 경험해보세요.',
   ogImage: 'https://xenialsoft.com/logo_light.png',
 });
